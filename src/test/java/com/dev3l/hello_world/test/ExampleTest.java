835c6da09b792a5dfc659757b591b8c98adab4fe
DEV3L <jus.beall@gmail.com>
2016-02-16 00:29:36 -0500

    Add a example test for continuous delivery

---
+++ src/test/java/com/dev3l/hello_world/test/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.dev3l.hello_world.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+	@Test
+	public void exampleTest() {
+		Assert.assertTrue(true);
+	}
+}
